9bde0ddd558bbd80864bef4c999881547b3a1d2b
jhrach700 <jhrach700@gmail.com>
2020-01-12 21:50:08 -0600

    setup for recyclerview

--- app/src/main/java/com/example/android/firstprogrammingassignment/DisplayActivity.kt
+++ app/src/main/java/com/example/android/firstprogrammingassignment/DisplayActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.firstprogrammingassignment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
@@ -12,16 +13,17 @@ class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
-        setSupportActionBar(toolbar)
+        recyclerview_main.setBackgroundColor(Color.BLUE)
+        //setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        //fab.setOnClickListener { view ->
+        //    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //        .setAction("Action", null).show()
+        //}
 
         var currentUser = intent.extras.getSerializable("Current_User")
         //Log.e("display:",currentUser.toString())
-        username_textview.setText("Logged in!!!")
+        //username_textview.setText("Logged in!!!")
 
 
 
