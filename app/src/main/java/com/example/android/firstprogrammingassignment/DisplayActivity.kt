f21ec1f68fe30737cd82995746e59964849b1a46
jhrach700 <jhrach700@gmail.com>
2020-01-19 18:53:41 -0600

    fetch json using okhttp correctly

--- app/src/main/java/com/example/android/firstprogrammingassignment/DisplayActivity.kt
+++ app/src/main/java/com/example/android/firstprogrammingassignment/DisplayActivity.kt
@@ -6,10 +6,13 @@ import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
 
 
 import kotlinx.android.synthetic.main.activity_display.*
 import layout.DisplayAdapter
+import okhttp3.*
+import java.io.IOException
 
 class DisplayActivity : AppCompatActivity() {
 
@@ -19,10 +22,39 @@ class DisplayActivity : AppCompatActivity() {
         recyclerview_main.layoutManager = LinearLayoutManager(this)
         recyclerview_main.adapter = DisplayAdapter()
         var currentUser = intent.extras.getSerializable("Current_User")
+        fetchJson()
+
     }
 
     fun fetchJson() {
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val req = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(req).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("onFailure:","Failed to execute request")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                //val body = response?.body()?.toString()
+                val body = response?.body()?.string()
+                Log.e("onResponse:","Successful request btw:" + body.toString())
+                println("Body btw:"+body)
+                val gson = GsonBuilder().create()
+                val feed = gson.fromJson(body, Feed::class.java)
+
+
+            }
+        })
+
 
     }
 
 }
+
+class Feed(val videos: List<Video>)
+
+class Video(val id: Int, val name: String, val link : String, val imageUrl : String, val numberOfViews : Int,
+            val channel: Channel)
+
+class Channel(val name: String, val profileImageUrl : String)
